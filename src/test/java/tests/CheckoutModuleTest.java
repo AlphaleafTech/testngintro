b6db72d3aaccfb06581715f7906dd8cf809e933d
Nijat Muhtar <nijatmn@gmail.com>
2022-12-06 20:27:26 -0500

    created test classes

---
+++ src/test/java/tests/CheckoutModuleTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class CheckoutModuleTest {
+
+    @Test
+    public void verifyUserCanCheckout() {
+
+    }
+
+    @Test
+    public void verifyCardDeclineMessage() {
+
+    }
+
+    @Test
+    public void verifyItemTotal() {
+
+    }
+
+    @Test
+    public void verifyCheckoutCancellation() {
+
+    }
+}
---
+++ src/test/java/tests/LoginModuleTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginModuleTest {
+
+    @Test
+    public void verifyUserCanLogin() {
+
+    }
+
+    @Test
+    public void verifyUserCanResetCredential() {
+
+    }
+
+    @Test
+    public void verifyUserLogout() {
+
+    }
+
+}//end
