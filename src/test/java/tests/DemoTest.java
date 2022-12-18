287e5f2200002f8f1eddde76bf149754c4cd57c0
Jonathan White <yggdrasil777@gmail.com>
2022-12-18 14:11:49 -0500

    Fixed for Jenkins

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -1,2 +1,11 @@
-package tests;public class DemoTest {
-}
+package tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void demoTestCase(){
+        System.out.println("Hello from Jonathan!!!");
+    }
+}//end::class
