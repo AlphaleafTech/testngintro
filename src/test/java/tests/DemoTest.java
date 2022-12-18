fe3b9fe5412cedd0ca43e74cc7f3e5c4a399f0e2
Nijat Muhtar <nijatmn@gmail.com>
2022-12-18 13:33:03 -0500

    latest

---
+++ src/test/java/tests/DemoTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void demoTest() {
+        System.out.println("-----HELLO-----");
+        System.out.println("TESTCASE IS BEING EXECUTED");
+        System.out.println("ALPHALEAF!!!!!!!");
+        System.out.println("-----END-------");
+    }
+}
