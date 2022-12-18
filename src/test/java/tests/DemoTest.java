e044db35ccdfecd539717f79b48be1a1112a6e94
Jonathan White <yggdrasil777@gmail.com>
2022-12-18 14:16:06 -0500

    Updated code

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -7,5 +7,7 @@ public class DemoTest {
     @Test
     public void demoTestCase(){
         System.out.println("Hello from Jonathan!!!");
+        System.out.println("This is a demo test run.");
+        System.out.println("Using Jenkins to demonstrate code change.");
     }
 }//end::class
